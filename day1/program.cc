dd28412a4910f763d2b7cb78ff026bac9295e9ad
Simon Watts <14802483+siwatts@users.noreply.github.com>
2025-12-01 11:57:19 +0000

    Add day 1 template

---
+++ day1/program.cc
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    cout << "--\nAoC 2025 Day 1\n--\n";
+    // For testing
+    int debuglimit = 1;
+    int debug = 0;
+    bool debugapply = true;
+
+    // User args
+    string filename = "input";
+    if (argc == 1) {
+        cout << "Assume default input file '" << filename << "'\n";
+    }
+    else if (argc > 1) {
+        filename = argv[1];
+        cout << "Taking CLI input file name '" << filename << "'\n";
+    }
+    if (argc > 2) {
+        cout << "Reading 2nd input param\n    -d / --debug for debug printing\n";
+        if (string{argv[2]} == "-d" || string{argv[2]} == "--debug") {
+            debugapply = true;
+        }
+        else {
+            debugapply = false;
+        }
+    }
+    if (debugapply) {
+        cout << "--\nDEBUG MODE : ON\nLINE LIMIT : " << debuglimit << "\n--" << endl;
+    }
+
+    // Input file
+    ifstream input(filename);
+
+    // Variables for output
+    long sum = 0;
+
+    // Read file
+    string line;
+    while (getline(input, line) && (!debugapply || debug < debuglimit))
+    {
+        debug++;
+        if (debugapply) {
+            cout << line << endl;
+        }
+    }
+
+    // Finished with input file
+    input.close();
+
+    // Output
+    cout << "--\n";
+    cout << "Sum = " << sum << endl;
+
+    cout << "--\nEnd.\n";
+    return 0;
+}
+
