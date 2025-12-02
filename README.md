3ee056e7c456323617028cf16ff3bc6bcd14fd63
Simon Watts <14802483+siwatts@users.noreply.github.com>
2025-12-02 12:47:53 +0000

    Add C# boilerplate

--- README.md
+++ README.md
@@ -19,3 +19,36 @@ g++ program.cc -std=c++23 -Wall
 
 TODO: Make a single project to re-use code between days
 
+## C#
+
+### Setup
+
+Make a solution file at root level first
+
+```
+dotnet new sln --name advent-of-code-2025
+```
+
+### Add Day
+
+Create a new C# console app per day. This will create a subdir. of the same name
+so cd to where you want this to happen first
+
+```
+dotnet new console -n dayX
+```
+
+Add the newly created C# project to the top level `.sln` file for vscode functionality
+
+```
+dotnet sln advent-of-code-2024.sln add day10/day10-csharp/day10-csharp.csproj
+```
+
+### Run
+
+```sh
+cd dayX/
+dotnet run
+# Or dotnet build to only build
+```
+
---
+++ template.cs
@@ -0,0 +1,85 @@
+﻿using System.Diagnostics;
+using System.IO;
+
+namespace AOC
+{
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            Console.WriteLine("--\nAoC 2025 Day X\n--");
+            // For testing
+            int debuglimit = 10;
+            bool debugmode = false;
+
+            // User args
+            List<string> argv = args.ToList();
+            string filename = "input";
+            if (argv.Count == 0)
+            {
+                Console.WriteLine("Assume default input file '{0}'", filename);
+            }
+            else if (argv.Count > 0)
+            {
+                filename = argv[0];
+                Console.WriteLine("Taking CLI input file name '{0}'", filename);
+            }
+            if (argv.Count > 1)
+            {
+                Console.WriteLine("Reading 2nd input param\n    -d / --debug for debug printing");
+                if (argv[1] == "-d" || argv[1] == "--debug")
+                {
+                    debugmode = true;
+                }
+                else
+                {
+                    debugmode = false;
+                }
+            }
+            if (debugmode)
+            {
+                Console.WriteLine("--\nDEBUG MODE : ON\nLINE LIMIT : {0}", debuglimit);
+            }
+
+            // Variables for output
+            long sum = 0;
+
+            // Read file
+            String? line;
+            int lineNr = 0;
+            using (var streamReader = new StreamReader(filename))
+            {
+                while ((line = streamReader.ReadLine()) != null && (!debugmode || lineNr < debuglimit))
+                {
+                    if (debugmode)
+                    {
+                        Console.WriteLine(line);
+                    }
+
+                    lineNr++;
+                }
+            }
+
+            // Processing
+            if (debugmode)
+            {
+            }
+
+            // Output
+            Console.WriteLine("--");
+            Console.WriteLine("Sum = {0}", sum);
+            if (sum == 123)
+            {
+                Console.WriteLine("Answer matches example expected answer");
+            }
+            else
+            {
+                Console.WriteLine("Answer does not match example expected answer");
+            }
+
+            Console.WriteLine("--\nEnd.");
+            return 0;
+        }
+    }
+}
+
