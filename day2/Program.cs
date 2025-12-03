1412b41fadd5767b05f83635e98397fb5bf26754
Simon Watts <14802483+siwatts@users.noreply.github.com>
2025-12-03 14:06:04 +0000

    Add a generalised solution for day 2 P2

--- day2/Program.cs
+++ day2/Program.cs
@@ -33,6 +33,87 @@ namespace AOC
 
             return (left == right);
         }
+        public static bool IsInvalidIDPart2(long id)
+        {
+            // Split number, see if it is a repeat of the same digits
+            // Need to consider possibilities with more than 2 repeat
+            int digitCount = (int)Math.Log10(id) + 1;
+            if (digitCount == 1)
+            {
+                // Special case when 1 digit long, cannot repeat
+                return false;
+            }
+            List<short> digitList = new List<short>();
+            for (int i = 0; i < digitCount; i++)
+            {
+                // Strip digits to process multiple times
+                digitList.Add((short)(id % 10));
+                id /= 10;
+            }
+            // Digits added from smallest to largest, so reverse them
+            digitList.Reverse();
+
+            // Handle special case of fragment length 1 first
+            bool invalid = true;
+            // Could use digitList.Distinct().Count == 1, but for loop allows us to shortcut
+            // on first non-matching digit
+            long first = digitList.First();
+            foreach (short d in digitList)
+            {
+                if (first != d)
+                {
+                    invalid = false;
+                    break;
+                }
+            }
+            if (invalid)
+            {
+                return invalid;
+            }
+
+            // Now handle the other cases, we need to build fragments to compare
+            int halfDigitCount = digitCount / 2;
+            List<long> fragmentList;
+            for (int fragmentSize = 2; fragmentSize <= halfDigitCount; fragmentSize++)
+            {
+                // See if it divides cleanly
+                if (digitCount % fragmentSize == 0)
+                {
+                    // Proceed
+                    int fragmentCount = digitCount / fragmentSize;
+                    fragmentList = new List<long>();
+                    for (int fn = 0; fn < fragmentCount; fn++)
+                    {
+                        long fragment = 0;
+                        for (int i = 0; i < fragmentSize; i++)
+                        {
+                            fragment *= 10;
+                            // Offset i by the digits we've already read in for past fragments
+                            fragment += digitList[i + (fn*fragmentSize)];
+                        }
+                        fragmentList.Add(fragment);
+                    }
+                    // Check this fragment list for invalid status
+                    first = fragmentList.First();
+                    invalid = true;
+                    foreach (long f in fragmentList)
+                    {
+                        if (first != f)
+                        {
+                            invalid = false;
+                            break;
+                        }
+                    }
+                    // Return early if we find one
+                    if (invalid)
+                    {
+                        return invalid;
+                    }
+                }
+            }
+
+            return invalid;
+        }
         public static int Main(string[] args)
         {
             Console.WriteLine("--\nAoC 2025 Day 2\n--");
@@ -71,15 +152,19 @@ namespace AOC
 
             // Variables for output
             long res = 0;
+            long resP2 = 0;
             // <<<< Expected output for testing >>>>
             long? exp;
+            long? expP2;
             if (filename == "test")
             {
                 exp = 1227775554;
+                expP2 = 4174379265;
             }
             else
             {
                 exp = null;
+                expP2 = null;
             }
 
             // Read file
@@ -111,6 +196,10 @@ namespace AOC
                             {
                                 res += id;
                             }
+                            if (IsInvalidIDPart2(id))
+                            {
+                                resP2 += id;
+                            }
                         }
                     }
 
@@ -126,10 +215,23 @@ namespace AOC
             // Output
             Console.WriteLine("--");
             Console.WriteLine("Res = {0}", res);
+            Console.WriteLine("ResP2 = {0}", resP2);
             if (exp != null)
             {
-                Console.WriteLine("Exp = {0}", res);
+                Console.WriteLine("Exp = {0}", exp);
                 if (res == exp)
+                {
+                    Console.WriteLine("Test: PASS");
+                }
+                else
+                {
+                    Console.WriteLine("Test: FAIL");
+                }
+            }
+            if (expP2 != null)
+            {
+                Console.WriteLine("ExpP2 = {0}", expP2);
+                if (resP2 == expP2)
                 {
                     Console.WriteLine("Test: PASS");
                 }
