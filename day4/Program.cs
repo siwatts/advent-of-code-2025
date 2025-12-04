860d2838726b7d8e4c8d57d1742f4b81d5e60b84
Simon Watts <14802483+siwatts@users.noreply.github.com>
2025-12-04 11:05:31 +0000

    Day 4 Part 1

--- day4/Program.cs
+++ day4/Program.cs
@@ -3,6 +3,86 @@ using System.IO;
 
 namespace AOC
 {
+    public class Warehouse
+    {
+        private List<List<char>> grid = new List<List<char>>();
+        private int maxX = -1;
+        private int maxY = -1;
+        private const char paperChar = '@';
+        public Warehouse()
+        {
+        }
+        public void AddRow(string line)
+        {
+            if (maxX != -1 && maxX != line.Length - 1)
+            {
+                throw new InvalidDataException("Line with different length " + line.Length + " added to grid of width " + maxX+1);
+            }
+            else if (maxX == -1)
+            {
+                maxX = line.Length - 1;
+            }
+
+            grid.Add(line.ToList<char>());
+            maxY++;
+        }
+        private bool IsPaperRoll(int x, int y)
+        {
+            if (x >= 0 && x <= maxX)
+            {
+                if (y >= 0 && y <= maxY)
+                {
+                    return (grid[y][x] == paperChar);
+                }
+            }
+            // Out of bounds returns false
+            return false;
+        }
+        private int NumberOfAdjacentPaperRolls(int x, int y)
+        {
+            // xxx, y-1, x-1 to  x+1
+            // xox,  y , x-1 AND x+1
+            // xxx, y+1, x-1 to  x+1
+            int sum = 0;
+            for (int i = x-1; i <= x+1; i++)
+            {
+                for (int j = y-1; j <= y+1; j++)
+                {
+                    // Skip the centre i = x, j = y
+                    if (i != x || j != y)
+                    {
+                        if (IsPaperRoll(i,j))
+                        {
+                            sum++;
+                        }
+                    }
+                }
+            }
+
+            return sum;
+        }
+        public int SumAccessiblePaperRolls()
+        {
+            int sum = 0;
+            for (int x = 0; x <= maxX; x++)
+            {
+                for (int y = 0; y <= maxY; y++)
+                {
+                    if (IsPaperRoll(x, y))
+                    {
+                        // See if it is accessible
+                        int num = NumberOfAdjacentPaperRolls(x, y);
+                        // This paper roll is accessible if it has < 4 neighbours
+                        if (num < 4)
+                        {
+                            sum++;
+                        }
+                    }
+                }
+            }
+            return sum;
+        }
+    }
     public class Program
     {
         public static int Main(string[] args)
@@ -49,13 +129,14 @@ namespace AOC
             long? expP2 = null;
             if (filename == "test")
             {
-                //expP1 = X;
+                expP1 = 13;
                 //expP2 = X;
             }
 
             // Read file
             String? line;
             int lineNr = 0;
+            Warehouse warehouse = new Warehouse();
             using (var streamReader = new StreamReader(filename))
             {
                 while ((line = streamReader.ReadLine()) != null && (!debugmode || lineNr < debuglimit))
@@ -66,6 +147,7 @@ namespace AOC
                     }
 
                     // <<<< Process line >>>>
+                    warehouse.AddRow(line);
 
                     lineNr++;
                 }
@@ -75,6 +157,7 @@ namespace AOC
             if (debugmode)
             {
             }
+            resP1 = warehouse.SumAccessiblePaperRolls();
 
             // Output
             Console.WriteLine("--");
